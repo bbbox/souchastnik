7226ec0dac41b39a48b1abb1c1f957d8576c26a2
Mihail Evsyukov <evsyukovmv@gmail.com>
2013-04-04 14:21:58 +0300

    start :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Souchastnik::Application.config.secret_key_base = 'dfcaa6458d0253baa7b1e9e07ead39c28d0b7049eba66366cc4497318a3dec76f01703234f45ae26a19949b1f93d64efd9b6ae4ff7ef2a070d84413281b1f4a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Souchastnik::Application.config.session_store :encrypted_cookie_store, key: '_souchastnik_session'
